0bb494d6735fa00c561f82d355c0fcd41753a16e
SpaceLuigii <hugo.deltour@viacesi.fr>
2016-11-04 23:35:00 +0100

    soustraction

--- main.c
+++ main.c
@@ -4,10 +4,20 @@
 int addition(int a, int b){
     return a+b;
 }
+
+int soustraction(int a, int b){
+    if(a>b){
+        return a-b;
+    }
+    else{
+        return b-a;
+    }
+}
 int main()
 {
 
-    printf("L’addition de a et de b vaut %d\n", addition(1,4));
+    printf("L addition de a et de b vaut: %d\n", addition(1,4));
+    printf("Le resultat de la soustraction est: %d\n", soustraction(1,4));
 
     return 0;
 }
